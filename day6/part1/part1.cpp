bd920f2441acae5a2fcf10ac95b8a5200546cc06
Jonas Kabelitz <jonas.kabelitz@belden.com>
2023-12-06 10:55:42 +0100

    Solved Day 6 - Part 1/2

--- day6/part1/part1.cpp
+++ day6/part1/part1.cpp
@@ -15,152 +15,58 @@
 
 #include <range/v3/all.hpp>
 
-class mapping {
+static auto get_values(const std::string& line) -> std::vector<std::uint32_t> {
+  auto result = std::vector<std::uint32_t>{};
 
-public:
+  auto colon = line.find_first_of(':');
 
-  mapping(std::uint32_t source_start, std::uint32_t destination_start, std::uint32_t range)
-  : _source_start{source_start},
-    _destination_start{destination_start},
-    _range{range} { }
-
-  auto operator()(std::uint32_t value) const -> std::uint32_t {
-    return _destination_start + (value - _source_start);
-  }
-
-  auto lower_bound() const -> std::uint32_t {
-    return _source_start;
-  }
-
-  auto upper_bound() const -> std::uint32_t {
-    return _source_start + _range - 1u;
-  }
-
-  auto operator<(const mapping& other) const -> bool {
-    return _source_start < other._source_start;
-  }
-
-private:
-
-  std::uint32_t _source_start;
-  std::uint32_t _destination_start;
-  std::uint32_t _range;
-
-}; // class mapping
-
-class map {
-
-public:
-
-  auto add_mapping(std::uint32_t source_start, std::uint32_t destination_start, std::uint32_t range) -> void {
-    _mappings.emplace(source_start, destination_start, range);
-  }
-
-  auto operator()(std::uint32_t value) -> std::uint32_t {
-    for (const auto& mapping : _mappings) {
-      if (value > mapping.upper_bound()) {
-        continue;
-      } else if (value < mapping.lower_bound()) {
-        return value;
-      } else {
-        return mapping(value);
-      }
-    }
-
-    return value;
-  }
-
-private:
-
-  std::set<mapping> _mappings;
-
-}; // class map
-
-static auto split(std::string string, std::string delimiter) -> std::vector<std::string> {
-  auto result = std::vector<std::string>{};
   auto last = std::size_t{0u};
   auto next = std::size_t{0u};
-  
-  while ((next = string.find(delimiter, last)) != std::string::npos) {
-    result.push_back(string.substr(last, next - last));
-    last = next + delimiter.size();
-  } 
 
-  result.push_back(string.substr(last));
+  auto parts = line.substr(colon + 1u);
+
+  while ((next = parts.find_first_not_of(' ', last)) != std::string::npos) {
+    last = parts.find_first_of(' ', next);
+    auto substr = parts.substr(next, last - next);
+
+    auto value = std::uint32_t{0};
+    std::from_chars(substr.data(), substr.data() + substr.size(), value);
+    result.push_back(value);
+  } 
 
   return result;
 }
 
 auto main() -> int {
-  auto input = std::ifstream{"day5/part1/input.txt"};
+  auto input = std::ifstream{"day6/part1/input.txt"};
+
+  auto result = std::uint32_t{1u}; 
 
   auto line = std::string{};
 
-  auto result = std::numeric_limits<std::uint32_t>::max();
+  std::getline(input, line);
 
-  auto seeds = std::vector<std::uint32_t>{};
-  auto maps = std::unordered_map<std::string, map>{};
+  const auto times = get_values(line);
 
-  while (std::getline(input, line)) {
-    if (line.empty()) {
-      continue;
-    }
+  std::getline(input, line);
 
-    auto colon_position = line.find_first_of(':');
+  const auto distances = get_values(line);
 
-    if (colon_position == std::string::npos) {
-      continue;
-    }
-    
-    auto type = line.substr(0u, colon_position);
+  for (const auto& [time, distance] : ranges::views::zip(times, distances)) {
+    auto sum = std::uint32_t{0u};
 
-    if (type == "seeds") {
-      auto values = split(line.substr(colon_position + 1u), " ");
+    for (auto held_time = 0u; held_time <= time; ++held_time) {
+      const auto time_to_race = time - held_time;
+      const auto velocity = held_time;
 
-      for (const auto& value : values) {
-        if (value.empty()) {
-          continue;
-        }
+      const auto current_distance = time_to_race * velocity;
 
-        auto val = std::uint32_t{};
-        auto result = std::from_chars(value.data(), value.data() + value.size(), val);
-
-        if (result.ec != std::errc::invalid_argument && result.ec != std::errc::result_out_of_range) {
-          seeds.push_back(val);
-        }
-      }
-    } else {
-      auto map_type = type.substr(0u, type.find_first_of(' '));
-
-      while (std::getline(input, line)) {
-        if (line.empty()) {
-          break;
-        }
-
-        auto dest_start = std::uint32_t{};
-        auto src_start = std::uint32_t{};
-        auto range = std::uint32_t{};
-        auto stream = std::stringstream{line};
-
-        stream >> dest_start >> src_start >> range;
-
-        maps[map_type].add_mapping(src_start, dest_start, range);
+      if (current_distance > distance) {
+        ++sum;
       }
     }
-  }
 
-  for (const auto& seed : seeds) {
-    auto value = seed;
-
-    value = maps["seed-to-soil"].operator()(value);
-    value = maps["soil-to-fertilizer"].operator()(value);
-    value = maps["fertilizer-to-water"].operator()(value);
-    value = maps["water-to-light"].operator()(value);
-    value = maps["light-to-temperature"].operator()(value);
-    value = maps["temperature-to-humidity"].operator()(value);
-    value = maps["humidity-to-location"].operator()(value);
-
-    result = std::min(result, value);
+    result *= sum;
   }
 
   fmt::print("Result: {}\n", result);
